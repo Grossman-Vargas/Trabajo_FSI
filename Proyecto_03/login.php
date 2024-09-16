c600111b24237497deb1d80494d19a3de83529d8
Grossman Vargas Pérez <131701814+Grossman-Vargas@users.noreply.github.com>
2024-09-15 21:34:48 -0500

    Update login.php

--- Proyecto_03/login.php
+++ Proyecto_03/login.php
@@ -8,19 +8,19 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $username = $_POST['username'];
     $password = $_POST['password'];
 
-    // Escapar el username para prevenir SQL injection
-    $username = mysqli_real_escape_string($con, $username);
-    $password = mysqli_real_escape_string($con, $password); // También escapamos la contraseña
 
-    // Buscar el usuario en la base de datos
+    $username = mysqli_real_escape_string($con, $username);
+    $password = mysqli_real_escape_string($con, $password); 
+
+
     $sql = "SELECT * FROM users WHERE username='$username' AND password='$password'";
     $query = mysqli_query($con, $sql);
 
-    // Verificar si se encontró el usuario
+
     if (mysqli_num_rows($query) > 0) {
         $row = mysqli_fetch_assoc($query);
 
-        // Iniciar sesión y redirigir a la página de gestión de libros
+     
         $_SESSION['user_id'] = $row['id'];
         $_SESSION['username'] = $row['username'];
 
